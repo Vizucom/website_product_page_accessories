730ffd47ad928d1f75cef380830a353101124c76
Timo Talvitie <timo.talvitie@vizucom.com>
2015-07-30 11:04:30 +0300

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   Copyright (c) 2015- Vizucom Oy (http://www.vizucom.com)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Website - Accessories on Product Page',
+    'category': 'Website',
+    'version': '0.1',
+    'author': 'Vizucom Oy',
+    'website': 'http://www.vizucom.com',
+    'depends': ['website_sale'],
+    'description': """
+Website - Accessories on Product Page
+=====================================
+ * Shows the suggested accessory products on the product page after suggested alternatives
+
+""",
+    'data': [
+        'views/product_accessories.xml',
+    ]
+    
+}
